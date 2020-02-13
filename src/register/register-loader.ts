3c5337e6e44e52c6a16207f54a06366fcd23207d
justinc <justinc@buildingiq.com>
2020-02-13 21:20:45 +1100

    Test data file to work with and docs of the format we'll be parsing.

--- src/register/register-loader.ts
+++ src/register/register-loader.ts
@@ -5,5 +5,11 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+/**
+ * Loader for the CASA aircraft register files. These are CSV files with the
+ * column format defined here:
+ * https://www.casa.gov.au/standard-page/data-files-field-definitions
+ */
 export class RegisterLoader {
 }
